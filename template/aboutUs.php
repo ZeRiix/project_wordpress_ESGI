b97d086e8a3be86a48491b16b54af13a795e1ff0
zeriix <willflo46@gmail.com>
2023-07-13 20:36:05 +0200

    [ADD]: aboutUs, services, partners, bog page

---
+++ template/aboutUs.php
@@ -0,0 +1,112 @@
+<?php get_header(); ?>
+<div class="w-[100%] h-[3330px] relative">
+    <div class="w-[1604px] h-[852px] left-[156px] top-[2478px] absolute">
+        <div class="w-[364px] h-[710px] left-[1240px] top-[142px] absolute">
+            <div class="w-[72px] h-11 left-0 top-[566px] absolute justify-center items-center inline-flex">
+                <div class="text-slate-900 text-[35px] font-semibold">CEO</div>
+            </div>
+            <div class="w-52 h-[77px] left-0 top-[633px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
+                +33 1 53 31 25 25<br />ceo@company.com</div>
+            <div class="w-[364px] h-[531px] left-0 top-0 absolute">
+                <img class="w-[363px] h-[531px] left-[0.50px] top-0 absolute"
+                    src="https://via.placeholder.com/363x531" />
+                <img class="w-[363px] h-[531px] left-[0.50px] top-0 absolute"
+                    src="https://via.placeholder.com/363x531" />
+            </div>
+        </div>
+        <div class="w-[366px] h-[710px] left-[825px] top-[142px] absolute">
+            <div class="w-[147px] h-11 pl-0.5 left-0 top-[566px] absolute justify-end items-center inline-flex">
+                <div class="text-slate-900 text-[35px] font-semibold">Designer</div>
+            </div>
+            <div class="w-52 h-[77px] left-[2px] top-[633px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
+                +33 1 53 31 25 20<br />design@company.com</div>
+            <div class="w-[363px] h-[531px] left-[3px] top-0 absolute">
+                <img class="w-[363px] h-[531px] left-0 top-0 absolute" src="https://via.placeholder.com/363x531" />
+                <img class="w-[363px] h-[531px] left-0 top-0 absolute" src="https://via.placeholder.com/363x531" />
+            </div>
+        </div>
+        <div class="w-[365px] h-[710px] left-[414px] top-[142px] absolute">
+            <div class="w-52 h-[77px] left-[2px] top-[633px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
+                +33 1 53 31 25 24<br />plan@company.com</div>
+            <div
+                class="w-[253px] h-11 pl-0.5 pr-[25px] left-0 top-[566px] absolute justify-start items-center inline-flex">
+                <div class="text-slate-900 text-[35px] font-semibold">Event planner</div>
+            </div>
+            <div class="w-[363px] h-[531px] left-[2px] top-0 absolute">
+                <img class="w-[363px] h-[531px] left-0 top-0 absolute" src="https://via.placeholder.com/363x531" />
+                <img class="w-[363px] h-[531px] left-0 top-0 absolute" src="https://via.placeholder.com/363x531" />
+            </div>
+        </div>
+        <div class="w-[365px] h-[710px] left-[2px] top-[142px] absolute">
+            <div class="w-52 h-[77px] left-[2px] top-[633px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
+                +33 1 53 31 25 23<br />sales@company.com</div>
+            <div class="w-[245px] h-11 pt-1 left-0 top-[562px] absolute justify-center items-center inline-flex">
+                <div class="text-slate-900 text-[35px] font-semibold">Sales Manager</div>
+            </div>
+            <div class="w-[363px] h-[531px] left-[2px] top-0 absolute">
+                <img class="w-[363px] h-[531px] left-0 top-0 absolute" src="https://via.placeholder.com/363x531" />
+                <img class="w-[363px] h-[531px] left-0 top-0 absolute" src="https://via.placeholder.com/363x531" />
+            </div>
+        </div>
+        <div class="w-[490px] h-[79px] pr-[181px] left-0 top-0 absolute justify-start items-center inline-flex">
+            <div class="text-slate-900 text-[63px] font-semibold tracking-[3px]">Our Team</div>
+        </div>
+    </div>
+    <div class="w-[1484px] h-[1350px] left-0 top-[996px] absolute">
+        <div class="w-[639px] h-[651px] left-[845px] top-[534px] absolute">
+            <div class="w-[639px] h-[193px] left-0 top-[458px] absolute">
+                <div
+                    class="w-[638px] h-[114px] left-[1px] top-[79px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
+                    Vivamus et viverra neque, ut pharetra ipsum. Aliquam eget consequat libero, quis cursus tortor.
+                    Aliquam suscipit eros sit amet velit malesuada dapibus. Fusce in vehicula tellus. Donec quis
+                    lorem ut magna tincidunt egestas. </div>
+                <div class="w-[262px] h-[59px] left-0 top-0 absolute justify-center items-center inline-flex">
+                    <div class="text-slate-900 text-[47px] font-semibold">Our mission</div>
+                </div>
+            </div>
+            <div class="w-[638px] h-48 left-[1px] top-[209px] absolute">
+                <div
+                    class="w-[638px] h-[113px] left-0 top-[79px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
+                    Nullam faucibus interdum massa. Duis eget leo mattis, pulvinar nisi et, consequat lectus.
+                    Suspendisse commodo magna orci, id luctus risus porta pharetra. Fusce vehicula aliquet urna non
+                    ultricies.</div>
+                <div class="w-[222px] h-[59px] left-0 top-0 absolute justify-center items-center inline-flex">
+                    <div class="text-slate-900 text-[47px] font-semibold">Our vision</div>
+                </div>
+            </div>
+            <div class="w-[638px] h-[153px] left-[1px] top-0 absolute">
+                <div
+                    class="w-[638px] h-[75px] left-0 top-[78px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu convallis elit, at
+                    convallis magna.</div>
+                <div class="w-[293px] h-[59px] pr-px left-0 top-0 absolute justify-center items-center inline-flex">
+                    <div class="text-slate-900 text-[47px] font-semibold">Who are we?</div>
+                </div>
+            </div>
+        </div>
+        <div class="w-[660px] h-[990px] left-0 top-[360px] absolute">
+            <img class="w-[660px] h-[990px] left-0 top-0 absolute" src="https://via.placeholder.com/660x990" />
+            <img class="w-[660px] h-[990px] left-0 top-0 absolute" src="https://via.placeholder.com/660x990" />
+        </div>
+        <div class="w-[917px] h-[235px] left-[430px] top-0 absolute">
+            <div
+                class="w-[914px] h-[140px] left-[3px] top-[95px] absolute text-zinc-500 text-[26px] font-normal leading-[42px]">
+                Specializing in the creation of exceptional events for private and corporate clients, we design,
+                plan and manage every project from conception to execution. </div>
+            <div class="w-[590px] h-[79px] pr-[142px] left-0 top-0 absolute justify-start items-center inline-flex">
+                <div class="text-slate-900 text-[63px] font-semibold tracking-[3px]">Sky’s the limit</div>
+            </div>
+        </div>
+    </div>
+    <div class="w-[1760px] h-[864px] left-[160px] top-0 absolute">
+        <div class="w-[376px] h-[105px] left-0 top-0 absolute justify-center items-center inline-flex">
+            <div class="text-slate-900 text-[84px] font-semibold">About us.</div>
+        </div>
+        <div class="w-[376px] h-[105px] left-[554px] top-[53px] absolute"></div>
+        <div class="w-[1487px] h-[631px] left-[273px] top-[233px] absolute">
+            <img class="w-[1487px] h-[631px] left-0 top-0 absolute" src="https://via.placeholder.com/1487x631" />
+            <img class="w-[1487px] h-[631px] left-0 top-0 absolute" src="https://via.placeholder.com/1487x631" />
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
---
+++ template/blog/sidebar.php
@@ -0,0 +1,98 @@
+<div class="w-[502px] h-[1437px] relative">
+    <div class="w-[165px] h-[95px] left-[1px] top-[1342px] absolute">
+        <div class="w-[41px] h-[29px] left-[124px] top-[66px] absolute">
+            <div class="w-[41px] h-[29px] left-0 top-0 absolute bg-stone-50"></div>
+            <div class="left-[8px] top-[7px] absolute text-neutral-400 text-[13px] font-bold">App</div>
+        </div>
+        <div class="w-[59px] h-[29px] left-[55px] top-[66px] absolute">
+            <div class="w-[59px] h-[29px] left-0 top-0 absolute bg-stone-50"></div>
+            <div class="left-[7px] top-[7px] absolute text-neutral-400 text-[13px] font-bold">Twitter</div>
+        </div>
+        <div class="w-[45px] h-[29px] left-0 top-[66px] absolute">
+            <div class="w-[45px] h-[29px] left-0 top-0 absolute bg-stone-50"></div>
+            <div class="left-[8px] top-[7px] absolute text-neutral-400 text-[13px] font-bold">Web</div>
+        </div>
+        <div class="w-12 h-[25px] pr-0.5 left-0 top-0 absolute justify-center items-center inline-flex">
+            <div class="text-slate-900 text-xl font-bold">Tags</div>
+        </div>
+    </div>
+    <div class="w-[174px] h-[326px] left-0 top-[953px] absolute">
+        <div
+            class="w-[99px] h-[38px] pr-px left-[1px] top-[288px] absolute justify-center items-center gap-4 inline-flex">
+            <div class="text-zinc-500 text-xl font-normal leading-[38px]">Creative</div>
+        </div>
+        <div class="w-[77px] h-[38px] left-[1px] top-[229px] absolute justify-center items-center gap-4 inline-flex">
+            <div class="text-zinc-500 text-xl font-normal leading-[38px]">HTML</div>
+        </div>
+        <div class="w-40 h-[38px] left-[1px] top-[170px] absolute justify-center items-center gap-4 inline-flex">
+            <div class="text-zinc-500 text-xl font-normal leading-[38px]">Graphic design</div>
+        </div>
+        <div
+            class="w-[173px] h-[38px] pr-0.5 left-[1px] top-[111px] absolute justify-center items-center gap-4 inline-flex">
+            <div class="text-zinc-500 text-xl font-normal leading-[38px]">Web developing</div>
+        </div>
+        <div class="w-[127px] h-[38px] left-[1px] top-[52px] absolute justify-center items-center gap-4 inline-flex">
+            <div class="text-zinc-500 text-xl font-normal leading-[38px]">Advertising</div>
+        </div>
+        <div class="w-[103px] h-[25px] left-0 top-0 absolute justify-center items-center inline-flex">
+            <div class="text-slate-900 text-xl font-bold">Categories</div>
+        </div>
+    </div>
+    <div class="w-[114px] h-[90px] left-[1px] top-[799px] absolute">
+        <div class="w-[114px] h-[38px] left-0 top-[52px] absolute justify-center items-center gap-4 inline-flex">
+            <div class="text-zinc-500 text-xl font-normal leading-[38px]">May 2022</div>
+        </div>
+        <div class="w-[83px] h-[25px] pr-px left-0 top-0 absolute justify-center items-center inline-flex">
+            <div class="text-slate-900 text-xl font-bold">Archives</div>
+        </div>
+    </div>
+    <div class="w-[501px] h-[554px] left-0 top-[174px] absolute">
+        <div class="w-[500px] h-[100px] left-[1px] top-[454px] absolute">
+            <div class="left-[116px] top-[84px] absolute text-zinc-400 text-[13px] font-normal">12 May, 2022</div>
+            <div class="w-[383px] left-[117px] top-0 absolute text-zinc-500 text-xl font-normal leading-[38px]">Mauris
+                venenatis fermentum pellentesque. </div>
+            <div class="w-[88px] h-[88px] left-0 top-[9px] absolute">
+                <img class="w-[88px] h-[88px] left-0 top-0 absolute" src="https://via.placeholder.com/88x88" />
+                <img class="w-[88px] h-[88px] left-0 top-0 absolute" src="https://via.placeholder.com/88x88" />
+            </div>
+        </div>
+        <div class="w-[500px] h-[100px] left-[1px] top-[321px] absolute">
+            <div class="left-[116px] top-[84px] absolute text-zinc-400 text-[13px] font-normal">12 May, 2022</div>
+            <div class="w-[383px] left-[117px] top-[-0px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
+                Mauris venenatis fermentum pellentesque. </div>
+            <div class="w-[88px] h-[88px] left-0 top-[9px] absolute">
+                <img class="w-[88px] h-[88px] left-0 top-0 absolute" src="https://via.placeholder.com/88x88" />
+                <img class="w-[88px] h-[88px] left-0 top-0 absolute" src="https://via.placeholder.com/88x88" />
+            </div>
+        </div>
+        <div class="w-[500px] h-[100px] left-[1px] top-[188px] absolute">
+            <div class="left-[116px] top-[84px] absolute text-zinc-400 text-[13px] font-normal">12 May, 2022</div>
+            <div class="w-[383px] left-[117px] top-[-0px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
+                Mauris venenatis fermentum pellentesque. </div>
+            <div class="w-[88px] h-[88px] left-0 top-[9px] absolute">
+                <img class="w-[88px] h-[88px] left-0 top-0 absolute" src="https://via.placeholder.com/88x88" />
+                <img class="w-[88px] h-[88px] left-0 top-0 absolute" src="https://via.placeholder.com/88x88" />
+            </div>
+        </div>
+        <div class="w-[498px] h-[104px] left-[1px] top-[52px] absolute">
+            <div class="left-[115px] top-[88px] absolute text-zinc-400 text-[13px] font-normal">12 May, 2022</div>
+            <div class="w-[383px] left-[115px] top-0 absolute text-zinc-500 text-xl font-normal leading-[38px]">Mauris
+                venenatis fermentum pellentesque. </div>
+            <div class="w-[88px] h-[88px] left-0 top-[13px] absolute">
+                <img class="w-[88px] h-[88px] left-0 top-0 absolute" src="https://via.placeholder.com/88x88" />
+                <img class="w-[88px] h-[88px] left-0 top-0 absolute" src="https://via.placeholder.com/88x88" />
+            </div>
+        </div>
+        <div class="w-[124px] h-[25px] pr-px left-0 top-0 absolute justify-center items-center inline-flex">
+            <div class="text-slate-900 text-xl font-bold">Recent posts</div>
+        </div>
+    </div>
+    <div class="w-[501px] h-[100px] left-[1px] top-0 absolute">
+        <img class="w-4 h-4 left-[485px] top-[67px] absolute" src="https://via.placeholder.com/16x16" />
+        <div class="left-[1px] top-[52px] absolute text-zinc-400 text-xl font-normal leading-[38px]">Type to search
+        </div>
+        <div class="pr-px left-0 top-0 absolute justify-center items-center inline-flex">
+            <div class="text-slate-900 text-xl font-bold">Search</div>
+        </div>
+    </div>
+</div>
