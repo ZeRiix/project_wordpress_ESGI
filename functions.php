9243ef3331841ada17a9dac3a59ba53c204c2f5b
zeriix <willflo46@gmail.com>
2023-07-13 20:38:53 +0200

    [ADD]; Addinf functions

--- functions.php
+++ functions.php
@@ -1,17 +1,33 @@
 <?php
 
-function esgi_theme_support()
-{
+/**
+ * Register asset
+ */
+add_action('after_setup_theme', function () {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
-}
-
-
-function esgi_register_asset()
-{
+    add_theme_support('menus');
+    register_nav_menu('header', 'En tête du menu');
+    register_nav_menu('footer', 'Pied de page');
+});
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('style', get_stylesheet_uri());
     wp_register_script('taillwind', 'https://cdn.tailwindcss.com');
     wp_enqueue_script('taillwind');
+});
+
+/**
+ * Get js uri
+ */
+function js_uri()
+{
+    return get_theme_file_uri('/assets/js/');
 }
 
-add_action('after_setup_theme', 'esgi_theme_support');
-add_action('wp_enqueue_scripts', 'esgi_register_asset');
+/**
+ * Get img uri
+ */
+function img_uri()
+{
+    return get_theme_file_uri('/assets/images/');
+}
