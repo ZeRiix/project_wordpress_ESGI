75fb27bce941b28a4ef814769c3d1c373eae336f
Maubry94 <matthieu.aubry94370@gmail.com>
2023-07-16 01:41:03 +0200

    [REFACTO]: services page

--- template/services.php
+++ template/services.php
@@ -1,66 +1,46 @@
 <?php get_header(); ?>
-<div class="w-[100%] h-[1836px] relative">
-    <div class="w-[1800px] h-[991px] left-0 top-[845px] absolute">
-        <div class="w-[1800px] h-[631px] left-0 top-[360px] absolute">
-            <img class="w-[1800px] h-[631px] left-0 top-0 absolute" src="<?php img_uri(); ?>png/4.png" />
-        </div>
-        <div
-            class="w-[914px] h-[140px] left-[158px] top-[95px] absolute text-zinc-500 text-[26px] font-normal leading-[42px]">
-            Specializing in the creation of exceptional events for private and corporate clients, we design, plan
-            and manage every project from conception to execution. </div>
-        <div class="w-[481px] h-[79px] pr-[66px] left-[155px] top-0 absolute justify-start items-center inline-flex">
-            <div class="text-slate-900 text-[63px] font-semibold tracking-[3px]">Corp. Parties</div>
-        </div>
-    </div>
-    <div class="w-[1800px] h-[714px] left-0 top-0 absolute">
-        <div class="w-[480px] h-[480px] left-[1440px] top-[234px] absolute">
-            <img class="w-[480px] h-[480px] left-0 top-0 absolute" src="<?php img_uri(); ?>png/3.png" />
-        </div>
-        <div class="w-[480px] h-[480px] left-[960px] top-[234px] absolute">
-            <div class="w-[480px] h-[480px] left-0 top-0 absolute bg-zinc-300 bg-white"></div>
-            <div class="w-[480px] h-[480px] left-0 top-0 absolute bg-white">
-                <div
-                    class="w-[242px] h-[58px] left-[119px] top-[211px] absolute flex-col justify-center items-end gap-[9px] inline-flex">
-                    <div class="text-slate-900 text-[35px] font-semibold">Private Parties</div>
-                    <div class="w-60 h-[5px] bg-gradient-to-r from-orange-200 to-pink-400"></div>
-                </div>
-            </div>
-        </div>
-        <div class="w-[480px] h-[480px] left-[480px] top-[234px] absolute">
-            <img class="w-[480px] h-[480px] left-0 top-0 absolute" src="<?php img_uri(); ?>png/11.png" />
-            <div class="w-[480px] h-[480px] left-0 top-[234px] absolute">
-                <img class="w-[480px] h-[480px] left-0 top-0 absolute" src="<?php img_uri(); ?>png/12.png" />
-            </div>
-            <div class="w-[515px] h-[105px] left-[156px] top-0 absolute justify-center items-center inline-flex">
-                <div class="text-slate-900 text-[84px] font-semibold">Our Services.</div>
-            </div>
-        </div>
-    </div>
-    <?php get_footer(); ?>
 
+<main class="max-w-[1920px] pt-[135px] px-[30px] mx-auto">
+    <section class="mb-[32px] lg:mb-[65px] xl:mb-[130px] flex flex-col gap-[32px] lg:gap-[65px] xl:gap-[130px]">
+        <h1 class="ml-[30px] lg:ml-[50px] xl:ml-[150px] text-3xl lg:text-5xl xl:text-7xl font-semibold text-slate-900">Our Services</h1>
 
-    <?php
-    $the_query = new WP_Query(array(
-        'category_name' => 'aboutus',
-        'post_status' => 'publish',
-        'posts_per_page' => 1,
-    ));
+        <?php include 'includes/our-services.php' ?>
+    </section>
 
-    $the_query2 = new WP_Query(array(
-        'category_name' => 'aboutus',
-        'post_status' => 'publish',
-        'posts_per_page' => 1,
-    ));
+    <section class="mb-[32px] lg:mb-[65px] xl:mb-[130px]">
+        <h2 class="mb-[20px] text-2xl lg:text-4xl xl:text-6xl text-slate-900">Corp. Parties</h2>
 
-    if ($the_query->have_posts()) {
-        while ($the_query->have_posts()) {
-            $the_query->the_post();
-            echo '<h2>' . get_the_title() . '</h2>';
-            $test = explode('<!--more-->', get_the_content());
-            var_dump($test);
-        }
-    } else {
-        echo '<p>ntm</p>';
+        <p class="max-w-[900px] text-lg lg:text-xl xl:text-2xl text-zinc-500">
+            Specializing in the creation of exceptional events for private and corporate clients, we design, plan and manage every project from conception to execution. 
+        </p>
+    </section>
+
+    <section class="h-[200px] lg:h-[400px] xl:h-[600px] mx-[-30px] object-cover">
+        <img src="<?php echo img_uri(); ?>png/9.png" class="w-full h-full">
+    </section>
+</main>
+
+<?php get_footer(); ?>
+
+<?php
+$the_query = new WP_Query(array(
+    'category_name' => 'aboutus',
+    'post_status' => 'publish',
+    'posts_per_page' => 1,
+));
+
+$the_query2 = new WP_Query(array(
+    'category_name' => 'aboutus',
+    'post_status' => 'publish',
+    'posts_per_page' => 1,
+));
+
+if ($the_query->have_posts()) {
+    while ($the_query->have_posts()) {
+        $the_query->the_post();
+        echo '<h2>' . get_the_title() . '</h2>';
+        $test = explode('<!--more-->', get_the_content());
+        var_dump($test);
     }
-
-    ?>
+}
+?>
