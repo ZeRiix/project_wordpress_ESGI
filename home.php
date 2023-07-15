66a952b6ef64951a0222dadcb7bf2af66526abc2
Maubry94 <matthieu.aubry94370@gmail.com>
2023-07-16 00:46:25 +0200

    [REFACTO]: home page

--- home.php
+++ home.php
@@ -1,106 +1,99 @@
 <?php get_header(); ?>
 
-<div class="w-[100%] h-[3996px] relative">
-    <!-- START Partners SECTION -->
-    <div class="w-[100%] h-[249px] left-[156px] top-[3447px] absolute">
-        <div class="w-[100%] h-[106px] left-[5px] top-[143px] absolute">
-            <img class="w-[154px] h-[62px] left-[1047px] top-[22px] absolute opacity-60"
-                src="<?php echo img_uri() ?>svg/partner-5.svg" />
-            <img class="w-[154px] h-[62px] left-[800px] top-[22px] absolute opacity-60"
-                src="<?php echo img_uri() ?>svg/partner-4.svg" />
-            <img class="w-[172px] h-[59px] left-[516px] top-[24px] absolute opacity-60"
-                src="<?php echo img_uri() ?>svg/partner-3.svg" />
-            <img class="w-[187px] h-11 left-[210px] top-[31px] absolute"
-                src="<?php echo img_uri() ?>svg/partner-2.svg" />
-            <img class="w-[91px] h-[106px] left-0 top-0 absolute opacity-60"
-                src="<?php echo img_uri() ?>svg/partner-1.svg" />
+<main class="max-w-[1920px] pt-[135px] px-[30px] pb-[30px] mx-auto">
+    <!-- Hero section -->
+    <section class="ml-[30px] lg:ml-[50px] xl:ml-[150px] mb-[32px] lg:mb-[65px] xl:mb-[130px] flex flex-col gap-[58px] lg:gap-[115px] xl:gap-[230px]">
+        <h1 class="text-3xl lg:text-5xl xl:text-7xl font-semibold text-slate-900">A really professional structure<br>for all your events!</h1>
+
+        <div class="w-full max-w-[1490px] self-end">
+            <img src="<?php echo img_uri() ?>png/1.png">
         </div>
-        <div class="w-[400px] h-[79px] left-0 top-0 absolute justify-end items-center inline-flex">
-            <div class="text-slate-900 text-[63px] font-semibold tracking-[3px]">Our Partners</div>
+    </section>
+
+    <!-- About section -->
+    <section class="mb-[32px] lg:mb-[65px] xl:mb-[130px] flex flex-col gap-[32px] lg:gap-[65px] xl:gap-[130px]">
+        <div class="w-full max-w-[1490px] ml-[30px] lg:ml-[50px] xl:ml-[150px] self-end">
+            <div class="max-w-[900px]">
+                <h2 class="mb-[20px] text-2xl lg:text-4xl xl:text-6xl text-slate-900">About Us</h2>
+
+                <p class="text-lg lg:text-xl xl:text-2xl text-zinc-500">
+                    Specializing in the creation of exceptional events for private and corporate clients, we design, plan and manage every project from conception to execution. 
+                </p>
+            </div>
         </div>
-    </div>
-    <!-- END Partners SECTION -->
-    <!-- START Services SECTION -->
-    <div class="w-[100%] h-[623px] left-0 top-[2693px] absolute">
-        <div class="w-[360px] h-[360px] left-[1050px] top-[143px] absolute">
-            <img class="w-[360px] h-[360px] left-0 top-0 absolute" src="<?php echo img_uri() ?>png/3.png" />
+
+        <div class="w-full max-w-[1490px] mr-[30px] lg:mr-[50px] xl:mr-[150px] flex flex-col lg:flex-row gap-[32px] lg:gap-0 justify-between items-center">
+            <div class="w-full max-w-[660px] dis">
+                <img src="<?php echo img_uri() ?>png/2.png">
+            </div>
+
+            <ul class="max-w-[640px] flex flex-col gap-[60px]">
+                <li>
+                    <h3 class="mb-[20px] text-xl lg:text-3xl xl:text-5xl text-slate-900">Who are we?</h3>
+                    
+                    <p class="text-base lg:text-lg xl:text-xl text-zinc-500">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu convallis elit, at convallis magna.
+                    </p>
+                </li>
+
+                <li>
+                    <h3 class="mb-[20px] text-xl lg:text-3xl xl:text-5xl text-slate-900">Our vision</h3>
+
+                    <p class="text-base lg:text-lg xl:text-xl text-zinc-500">
+                        Nullam faucibus interdum massa. Duis eget leo mattis, pulvinar nisi et, consequat lectus. Suspendisse commodo magna orci, id luctus risus porta pharetra. Fusce vehicula aliquet urna non ultricies.
+                    </p>
+                </li>
+
+                <li>
+                    <h3 class="mb-[20px] text-xl lg:text-3xl xl:text-5xl text-slate-900">Our mission</h3>
+
+                    <p class="text-base lg:text-lg xl:text-xl text-zinc-500">
+                        Vivamus et viverra neque, ut pharetra ipsum. Aliquam eget consequat libero, quis cursus tortor. Aliquam suscipit eros sit amet velit malesuada dapibus. Fusce in vehicula tellus. Donec quis lorem ut magna tincidunt egestas. 
+                    </p>
+                </li>
+            </ul>
         </div>
-        <div class="w-[360px] h-[360px] left-[710px] top-[143px] absolute">
-            <div class="w-[360px] h-[360px] left-0 top-0 absolute">
-                <div class="w-[360px] h-[360px] left-0 top-0 absolute bg-white"></div>
-                <div
-                    class="w-[242px] h-[55px] left-[69px] top-[163px] absolute flex-col justify-center items-end gap-1.5 inline-flex">
-                    <div class="text-slate-900 text-[35px] font-semibold">Private Parties</div>
-                    <div class="w-60 h-[5px] bg-gradient-to-r from-orange-200 to-pink-400">&nbsp;</div>
+    </section>
+
+    <!-- Services section -->
+    <section class=" mb-[32px] lg:mb-[65px] xl:mb-[130px]">
+        <h2 class="ml-[30px] lg:ml-[50px] xl:ml-[150px] mb-[64px] text-2xl lg:text-4xl xl:text-6xl text-slate-900">Our Services</h2>
+    
+        <div class="lg:max-h-[480px] grid grid-rows-4 grid-cols-1 lg:grid-cols-4 lg:grid-rows-1">
+            <img src="<?php echo img_uri() ?>png/12.png" class="mx-auto aspect-square">
+
+            <img src="<?php echo img_uri() ?>png/11.png" class="mx-auto aspect-square">
+
+            <div class="flex justify-center items-center" class="mx-auto aspect-square">
+                <div>
+                    <h4 class="mb-[4px] text-lg lg:text-2xl xl:text-4xl text-slate-900">Private Parties</h4>
+
+                    <div class="w-full h-[4px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
                 </div>
             </div>
+
+            <img src="<?php echo img_uri() ?>png/3.png" class="mx-auto aspect-square">
         </div>
-        <div class="w-[360px] h-[360px] left-[360px] top-[143px] absolute">
-            <img class="w-[360px] h-[360px] left-0 top-0 absolute" src="<?php echo img_uri() ?>png/11.png" />
+    </section>
+
+    <!-- Partners section -->
+    <section class=" mb-[32px] lg:mb-[65px] xl:mb-[130px]">
+        <h2 class="ml-[30px] lg:ml-[50px] xl:ml-[150px] mb-[64px] text-2xl lg:text-4xl xl:text-6xl text-slate-900">Our Partners</h2>
+
+        <div class="flex flex-wrap justify-center gap-[120px]">
+            <img src="<?php echo img_uri() ?>svg/partner-1.svg">
+
+            <img src="<?php echo img_uri() ?>svg/partner-2.svg">
+
+            <img src="<?php echo img_uri() ?>svg/partner-3.svg">
+
+            <img src="<?php echo img_uri() ?>svg/partner-4.svg">
+
+            <img src="<?php echo img_uri() ?>svg/partner-5.svg">
+
+            <img src="<?php echo img_uri() ?>svg/partner-6.svg">
         </div>
-        <div class="w-[360px] h-[360px] left-0 top-[143px] absolute">
-            <img class="w-[360px] h-[360px] left-0 top-0 absolute" src="<?php echo img_uri() ?>png/12.png" />
-        </div>
-        <div class="w-[409px] h-[79px] pr-[3px] left-[157px] top-0 absolute justify-center items-center inline-flex">
-            <div class="text-slate-900 text-[63px] font-semibold tracking-[3px]">Our Services</div>
-        </div>
-    </div>
-    <!-- END Services SECTION -->
-    <!-- START AboutUs SECTION -->
-    <div class="w-[100%] h-[1350px] left-0 top-[1211px] absolute">
-        <div class="w-[639px] h-[652px] left-[745px] top-[529px] absolute">
-            <div class="w-[639px] h-[193px] left-0 top-[459px] absolute">
-                <div
-                    class="w-[638px] h-[114px] left-[1px] top-[79px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
-                    Vivamus et viverra neque, ut pharetra ipsum. Aliquam eget consequat libero, quis cursus tortor.
-                    Aliquam suscipit eros sit amet velit malesuada dapibus. Fusce in vehicula tellus. Donec quis
-                    lorem ut magna tincidunt egestas. </div>
-                <div class="w-[262px] h-[59px] left-0 top-0 absolute justify-center items-center inline-flex">
-                    <div class="text-slate-900 text-[47px] font-semibold">Our mission</div>
-                </div>
-            </div>
-            <div class="w-[639px] h-48 left-0 top-[210px] absolute">
-                <div
-                    class="w-[638px] h-[113px] left-[1px] top-[79px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
-                    Nullam faucibus interdum massa. Duis eget leo mattis, pulvinar nisi et, consequat lectus.
-                    Suspendisse commodo magna orci, id luctus risus porta pharetra. Fusce vehicula aliquet urna non
-                    ultricies.</div>
-                <div class="w-[222px] h-[59px] left-0 top-0 absolute justify-center items-center inline-flex">
-                    <div class="text-slate-900 text-[47px] font-semibold">Our vision</div>
-                </div>
-            </div>
-            <div class="w-[638px] h-[154px] left-[1px] top-0 absolute">
-                <div
-                    class="w-[638px] h-[75px] left-0 top-[79px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu convallis elit, at
-                    convallis magna.</div>
-                <div class="w-[293px] h-[59px] pr-px left-0 top-0 absolute justify-center items-center inline-flex">
-                    <div class="text-slate-900 text-[47px] font-semibold">Who are we?</div>
-                </div>
-            </div>
-        </div>
-        <div class="w-[560px] h-[990px] left-0 top-[360px] absolute">
-            <img class="w-[560px] h-[990px] left-0 top-0 absolute" src="<?php echo img_uri() ?>png/2.png" />
-        </div>
-        <div class="w-[914px] h-[221px] left-[433px] top-0 absolute">
-            <div class="w-[914px] left-0 top-[95px] absolute text-zinc-500 text-[26px] font-normal leading-[42px]">
-                Specializing in the creation of exceptional events for private and corporate clients, we design,
-                plan and manage every project from conception to execution. </div>
-            <div class="w-[300px] h-[79px] pr-[3px] left-[1px] top-0 absolute justify-center items-center inline-flex">
-                <div class="text-slate-900 text-[63px] font-semibold tracking-[3px]">About Us</div>
-            </div>
-        </div>
-    </div>
-    <!-- END AboutUs SECTION -->
-    <!-- START Home Section -->
-    <div class="w-[100%] h-[1079px] left-[156px] top-0 absolute">
-        <div class="w-[987px] h-[631px] right-[150px] top-[448px] absolute">
-            <img class="w-[987px] h-[481px] top-0 absolute" src="<?php echo img_uri() ?>png/1.png" />
-        </div>
-        <div class="w-[1000px] h-[221px] left-[60px] absolute text-slate-900 text-[74px] font-semibold">A
-            really
-            professional structure for all your events!</div>
-    </div>
-    <!-- END Home SECTION -->
-</div>
+    </section>
+</main>
+
 <?php get_footer(); ?>
