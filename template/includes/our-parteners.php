cf781c850dbd9e28fe177124898d1d2574133a4b
Maubry94 <matthieu.aubry94370@gmail.com>
2023-07-16 12:01:24 +0200

    [REFACTO]: parteners page

--- template/includes/our-parteners.php
+++ template/includes/our-parteners.php
@@ -1,5 +1,3 @@
-<h2 class="ml-[30px] lg:ml-[50px] xl:ml-[150px] mb-[64px] text-2xl lg:text-4xl xl:text-6xl text-slate-900">Our Partners</h2>
-
 <div class="flex flex-wrap justify-center gap-[120px]">
     <img src="<?php echo img_uri() ?>svg/partner-1.svg">
 
