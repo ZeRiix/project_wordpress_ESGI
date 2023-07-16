525f76bb0024c4e46c18ea26615879ef4665d9a7
ZeRiix <willflo46@gmail.com>
2023-07-16 18:35:18 +0200

    [FIX]: fix affichage partner in home page

--- template/includes/our-parteners.php
+++ template/includes/our-parteners.php
@@ -1,5 +1,7 @@
 <div class="flex flex-wrap justify-center gap-[120px]">
     <?php foreach (esgi_get_partners() as $partner) {
-        echo '<img src="' . $partner . '">';
+        if ($partner != false || $partner !== "") {
+            echo '<img src="' . $partner . '" width="200">';
+        }
     } ?>
 </div>
