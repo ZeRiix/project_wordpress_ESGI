c468f6732d34e1d23d73249911df51758e0de5a6
ZeRiix <willflo46@gmail.com>
2023-07-16 17:38:28 +0200

    [ADD]: adding custom input header and footer

--- footer.php
+++ footer.php
@@ -1,36 +1,38 @@
-    <footer class="w-[100%] min-h-[380px] p-[30px] lg:p-[50px] xl:px-[150px] xl:py-[75px] flex flex-col justify-between gap-[30px] lg:gap-0 bg-slate-900">
+    <footer
+        class="w-[100%] min-h-[380px] p-[30px] lg:p-[50px] xl:px-[150px] xl:py-[75px] flex flex-col justify-between gap-[30px] lg:gap-0 bg-slate-900">
         <div class="flex flex-col lg:flex-row justify-between gap-[30px] lg:gap-0">
             <a href="/" class="w-[115px] lg:w-[230px] h-[35px] lg:h-[70px]">
-                <img src="<?php echo img_uri(); ?>svg/logo-white.svg" alt="logo" class="w-full h-full" />
+                <img src="<?php echo esgi_get_site_logo()['logo_white']; ?>" alt="logo" class="w-full h-full" />
             </a>
 
             <div class="flex flex-col lg:flex-row gap-[30px] lg:gap-[95px] text-white">
                 <div class="flex flex-col gap-[5px]">
                     <h5 class="mb-[20px] font-semibold">Manager</h5>
-                    
-                    <span>+33 1 53 31 25 23</span>
 
-                    <span>info@esgi.com</span>
+                    <span><?php echo esgi_get_services()['manager']['phone']; ?></span>
+
+                    <span><?php echo esgi_get_services()['manager']['email']; ?></span>
                 </div>
                 <div class="flex flex-col gap-[5px]">
                     <h5 class="mb-[20px] font-semibold">CEO</h5>
 
-                    <span>+33 1 53 31 25 25</span>
+                    <span><?php echo esgi_get_services()['ceo']['phone']; ?></span>
 
-                    <span>ceo@company.com</span>
+                    <span><?php echo esgi_get_services()['ceo']['email']; ?></span>
                 </div>
-            </div>        
+            </div>
         </div>
 
         <div class="flex justify-between gap-[30px]">
-            <div class="mt-[30px] text-white text-[15px] font-normal">&copy;<?= date("Y") ?> Figma Template by ESGI</div>
+            <div class="mt-[30px] text-white text-[15px] font-normal">&copy;<?= date("Y") ?> Figma Template by ESGI
+            </div>
 
             <div class="flex gap-[30px] mt-[30px]">
-                <a href="https://www.facebook.com/" target="_blank">
+                <a href="<?php echo esgi_get_social_networks()['facebook']['url'] ?>" target="_blank">
                     <img src="<?php echo img_uri(); ?>svg/facebook.svg" alt="facebook" class="w-[21px] h-[21px]" />
                 </a>
 
-                <a href="https://www.linkedin.com/" target="_blank">
+                <a href="<?php echo esgi_get_social_networks()['linkedin']['url'] ?>" target="_blank">
                     <img src="<?php echo img_uri(); ?>svg/linkedin.svg" alt="linkedin" class="w-[21px] h-[21px]" />
                 </a>
             </div>
@@ -38,17 +40,17 @@
     </footer>
 
     <script>
-        btnToggle.addEventListener("click", () => {
-            divMenu.style.top = "0";
+btnToggle.addEventListener("click", () => {
+    divMenu.style.top = "0";
 
-        });
+});
 
-        btnToggleMenu.addEventListener("click", () => {
-            divMenu.style.top = "-820px";
-        }); 
+btnToggleMenu.addEventListener("click", () => {
+    divMenu.style.top = "-820px";
+});
     </script>
 
     <?php wp_footer(); ?>
-</body>
+    </body>
 
-</html>
+    </html>
--- header.php
+++ header.php
@@ -12,17 +12,18 @@ $page = get_all_pages();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" src="style.css">
-    
-    <?php wp_head(); ?> 
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
     <header id="header" class="fixed w-full bg-white">
-        <div id="divSansMenu" class="w-[100%] h-[135px] px-[30px] lg:px-[50px] xl:px-[150px] flex items-center justify-between">
+        <div id="divSansMenu"
+            class="w-[100%] h-[135px] px-[30px] lg:px-[50px] xl:px-[150px] flex items-center justify-between">
             <a href="/" class="w-[140px] h-[42px]">
-                <img id="logo" src="<?php echo img_uri(); ?>svg/logo.svg" alt="logo" />
+                <img id="logo" src="<?php echo esgi_get_site_logo()['logo']; ?>" alt="logo" />
             </a>
-            
+
             <button id="btnToggle" class="w-10 px-[10px] flex flex-col gap-[2px]">
                 <div class="burger-line w-10 h-[3px] bg-slate-900"></div>
 
@@ -30,16 +31,21 @@ $page = get_all_pages();
             </button>
         </div>
 
-        <div id="divMenu" class="fixed z-50 top-[-820px] left-0 w-full px-[30px] pb-[80px] lg:pb-[160px] lg:px-[50px] xl:px-[150px] bg-slate-900 transition-all duration-500">
+        <div id="divMenu"
+            class="fixed z-50 top-[-820px] left-0 w-full px-[30px] pb-[80px] lg:pb-[160px] lg:px-[50px] xl:px-[150px] bg-slate-900 transition-all duration-500">
             <div class="w-[100%] h-[135px] flex items-center justify-between">
                 <a href="/" class="w-[140px] h-[42px]">
-                    <img src="<?php echo img_uri(); ?>svg/logo-white.svg" alt="logo" />
+                    <img src="<?php echo esgi_get_site_logo()['logo_white']; ?>" alt="logo" />
                 </a>
-                
-                <button id="btnToggleMenu" class="relative w-[16.99px] h-[16.99px]">
-                    <div class="w-[21px] h-[3px] left-[2.14px] top-[0.01px] absolute origin-top-left rotate-45 bg-white"></div>
 
-                    <div class="w-[21px] h-[3px] left-[16.97px] top-[2.12px] absolute origin-top-left rotate-[135deg] bg-white"></div>
+                <button id="btnToggleMenu" class="relative w-[16.99px] h-[16.99px]">
+                    <div
+                        class="w-[21px] h-[3px] left-[2.14px] top-[0.01px] absolute origin-top-left rotate-45 bg-white">
+                    </div>
+
+                    <div
+                        class="w-[21px] h-[3px] left-[16.97px] top-[2.12px] absolute origin-top-left rotate-[135deg] bg-white">
+                    </div>
                 </button>
             </div>
 
@@ -47,24 +53,35 @@ $page = get_all_pages();
                 <nav>
                     <ul class="flex flex-col items-end">
                         <li>
-                            <a href="/" class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">Home</a>
+                            <a href="/"
+                                class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">Home</a>
                         </li>
 
                         <li>
-                            <a href="<?php page_uri('aboutUs') ?>" class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">About Us</a>
+                            <a href="<?php page_uri('aboutUs') ?>"
+                                class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">About Us</a>
                         </li>
 
                         <li>
-                            <a href="<?php page_uri('services') ?>" class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">Services</a>
+                            <a href="<?php page_uri('services') ?>"
+                                class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">Services</a>
                         </li>
 
                         <li>
-                            <a href="<?php page_uri('partners') ?>" class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">Partners</a>
+                            <a href="<?php page_uri('blog/list') ?>"
+                                class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">Blog</a>
                         </li>
 
+                        <!-- <li>
+                            <a href="<?php page_uri('partners') ?>"
+                                class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">Partners</a>
+                        </li> -->
+
                         <li>
-                            <a href="<?php page_uri('contacts') ?>" class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">Contacts</a>
+                            <a href="<?php page_uri('contacts') ?>"
+                                class="text-white text-[20px] lg:text-[35px] xl:text-[70px] font-semibold">Contacts</a>
                         </li>
+
                     </ul>
                 </nav>
             </div>
@@ -72,8 +89,8 @@ $page = get_all_pages();
     </header>
 
     <script>
-        const btnToggle = document.getElementById("btnToggle");
-        const btnToggleMenu = document.getElementById("btnToggleMenu");
-        const divSansMenu = document.getElementById("divSansMenu");
-        const divMenu = document.getElementById("divMenu");
+    const btnToggle = document.getElementById("btnToggle");
+    const btnToggleMenu = document.getElementById("btnToggleMenu");
+    const divSansMenu = document.getElementById("divSansMenu");
+    const divMenu = document.getElementById("divMenu");
     </script>
