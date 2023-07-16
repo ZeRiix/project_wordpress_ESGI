623e7a7ceb887e2272c9ba9c320275719d88ca3b
Maubry94 <matthieu.aubry94370@gmail.com>
2023-07-16 14:47:13 +0200

    [REFATCO]: search page

--- search.php
+++ search.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
-<div class="w-[100%] h-[870px] relative">
+
+<!-- <div class="w-[100%] h-[870px] relative">
     <div class="w-[502px] h-[284px] left-[1102px] top-[586px] absolute">
         <div
             class="w-[500px] h-[114px] left-[2px] top-[170px] absolute text-zinc-500 text-xl font-normal leading-[38px]">
@@ -90,5 +91,70 @@
         <div class="w-[297px] h-[5px] left-[733px] top-[99px] absolute bg-gradient-to-r from-orange-200 to-pink-400">
         </div>
     </div>
-</div>
+</div> -->
+
+<main class="max-w-[1920px] pt-[135px] pb-[30px] mx-auto">
+    <section class="mb-[65px] xl:mb-[130px] px-[30px] lg:px-[50px] xl:px-[150px] flex flex-col gap-[32px] lg:gap-[65px] xl:gap-[130px]">
+        <h1 class="flex gap-[10px] text-3xl lg:text-5xl xl:text-7xl font-semibold text-slate-900">
+            Search results for:
+            
+            <div class="flex flex-col">
+                <span class="mb-[4px]"><?= get_search_query() ?></span>
+
+                <div class="w-full h-[4px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
+            </div>
+        </h1>
+
+        <div class="grid grid-rows-6 grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 gap-[50px]">
+            <article class="flex flex-col gap-[20px]">
+                <h4 class="mb-[10px] text-lg lg:text-xl xl:text-3xl text-slate-900">Suspendisse aliquet efficitur porttitor. In ornare varius.</h4>
+
+                <span class="text-lg lg:text-xl text-slate-900 font-bold">Uncategorized, July 5, 2022</span>
+
+                <p class="text-lg lg:text-xl text-zinc-500">Nulla hendrerit, mauris non convallis lacinia, quam quam eleifend massa, id fermentum magna tellus in augue. Vestibulum scelerisque mauris.</p>
+            </article>
+
+            <article class="flex flex-col gap-[20px]">
+                <h4 class="mb-[10px] text-lg lg:text-xl xl:text-3xl text-slate-900">Suspendisse aliquet efficitur porttitor. In ornare varius.</h4>
+
+                <span class="text-lg lg:text-xl text-slate-900 font-bold">Uncategorized, July 5, 2022</span>
+
+                <p class="text-lg lg:text-xl text-zinc-500">Nulla hendrerit, mauris non convallis lacinia, quam quam eleifend massa, id fermentum magna tellus in augue. Vestibulum scelerisque mauris.</p>
+            </article>
+
+            <article class="flex flex-col gap-[20px]">
+                <h4 class="mb-[10px] text-lg lg:text-xl xl:text-3xl text-slate-900">Suspendisse aliquet efficitur porttitor. In ornare varius.</h4>
+
+                <span class="text-lg lg:text-xl text-slate-900 font-bold">Uncategorized, July 5, 2022</span>
+
+                <p class="text-lg lg:text-xl text-zinc-500">Nulla hendrerit, mauris non convallis lacinia, quam quam eleifend massa, id fermentum magna tellus in augue. Vestibulum scelerisque mauris.</p>
+            </article>
+
+            <article class="flex flex-col gap-[20px]">
+                <h4 class="mb-[10px] text-lg lg:text-xl xl:text-3xl text-slate-900">Suspendisse aliquet efficitur porttitor. In ornare varius.</h4>
+
+                <span class="text-lg lg:text-xl text-slate-900 font-bold">Uncategorized, July 5, 2022</span>
+
+                <p class="text-lg lg:text-xl text-zinc-500">Nulla hendrerit, mauris non convallis lacinia, quam quam eleifend massa, id fermentum magna tellus in augue. Vestibulum scelerisque mauris.</p>
+            </article>
+
+            <article class="flex flex-col gap-[20px]">
+                <h4 class="mb-[10px] text-lg lg:text-xl xl:text-3xl text-slate-900">Suspendisse aliquet efficitur porttitor. In ornare varius.</h4>
+
+                <span class="text-lg lg:text-xl text-slate-900 font-bold">Uncategorized, July 5, 2022</span>
+
+                <p class="text-lg lg:text-xl text-zinc-500">Nulla hendrerit, mauris non convallis lacinia, quam quam eleifend massa, id fermentum magna tellus in augue. Vestibulum scelerisque mauris.</p>
+            </article>
+
+            <article class="flex flex-col gap-[20px]">
+                <h4 class="mb-[10px] text-lg lg:text-xl xl:text-3xl text-slate-900">Suspendisse aliquet efficitur porttitor. In ornare varius.</h4>
+
+                <span class="text-lg lg:text-xl text-slate-900 font-bold">Uncategorized, July 5, 2022</span>
+
+                <p class="text-lg lg:text-xl text-zinc-500">Nulla hendrerit, mauris non convallis lacinia, quam quam eleifend massa, id fermentum magna tellus in augue. Vestibulum scelerisque mauris.</p>
+            </article>
+        </div>
+    </section>
+</main>
+
 <?php get_footer(); ?>
